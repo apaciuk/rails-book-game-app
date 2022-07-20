a152953b11b16e89d45959ecd04ac269ea9dfaf2
Paul Anthony McGowan <info@xhostcom.com>
2022-07-20 21:15:07 +0100

    Validation Specs passed

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Word, type: :model do
     it { is_expected.to have_db_column(:value).of_type(:string) }
     it { is_expected.to have_db_column(:language).of_type(:string) }
   end
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to validate_presence_of(:language) }
+  end
 end
