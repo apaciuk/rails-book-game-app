6e171144fa4d30e2462ddc20419ea2a57697e071
Paul Anthony McGowan <info@xhostcom.com>
2022-07-21 17:12:19 +0100

    Post test 1, 13 tests all

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -27,4 +27,33 @@ RSpec.describe WordsController, type: :controller do
             get :index
             expect(response).to render_template('index')
         end
+    describe 'GET new' do 
+        before { get :new }
+        it 'returns http success' do 
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+        it 'renders the new template' do 
+            get :new
+            expect(response).to render_template('new')
+        end 
+
+        it 'assigns @word' do 
+            get :new
+            expect(assigns(:word)).to be_a_new(Word)
+        end 
+
+        it 'renders the new template' do 
+            get :new
+            expect(response).to render_template('new')
+        end
+    end 
+
+    describe 'POST create' do 
+    subject { post :create, params: { word: { value: 'word', language: 'en' } } } 
+
+    it 'creates a new word' do 
+        expect { subject }.to change(Word, :count).by(1)
+    end 
+end
 end
